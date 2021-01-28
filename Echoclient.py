655418e8bf65e974ac4bfc642d3286010f90004f
Ossi Heino <ossi.heino@gmail.com>
2021-01-29 00:29:24 +0200

    Server and client

--- Echoclient.py
+++ Echoclient.py
@@ -5,37 +5,40 @@ import argparse
 host = 'localhost'
 
 def echo_client(port):
-    #create tcp/ip socket and connect it to the server
+    # Create tcp/ip socket and connect it to the server
     sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    server_address = (host,port)
-    sock.settimeout(4000)
-    print("Connecting to %s port %s"%server_address)
+    server_address = (host, port)
+    sock.settimeout(4)
+
+    print("Connecting to %s port %s" % server_address)
     sock.connect(server_address)
+
     while True:
-        # send data
+        # Send data
         try:
-            # ask for message to send, convert it to bytes and send it.
+            # Message to send, convert it to bytes and send it.
             message = str(input("Type message to send: "))
             message_to_bytes = str.encode(message)
             type(message_to_bytes)
 
-            print("Sending: %s" %message)
+            print("Sending: %s" % message)
             sock.sendall(message_to_bytes)
 
-            # wait for response
+            # Wait for response
             amount_received = 0
             amount_expected = len(message_to_bytes)
+
             while amount_received < amount_expected:
                 data = sock.recv(16)
                 amount_received += len(data)
 
-                # convert received bytes back to string and print it
+                # Convert received bytes back to string
                 data_to_string = data.decode()
                 print("Received: %s" % data_to_string)
 
         except Exception as e:
             print("Error: %s" %str(e))
-    #finally:
+
     print("Closing connection")
     sock.close()
 
--- Echoserver.py
+++ Echoserver.py
@@ -11,44 +11,51 @@ data_payload = 2048
 backlog = 5
 
 def echo_server(port):
-    # Tcp socket
+    # TCP socket
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # enable reuse address/port
+
+    # Enable reuse address/port
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)
-    # bind socket 2 port
+
+    # Bind socket 2 port
     server_address = (host,port)
-    print("Starting echo server on %s port %s" %server_address)
+    print("Starting echo server on %s port %s" % server_address)
     sock.bind(server_address)
 
-    # listen to clients, max # of queued connections = backlog
+    # Listen to clients, max # of queued connections = backlog
     sock.listen(backlog)
+
     while True:
         print("Waiting for a message from client")
         client, address = sock.accept()
-        # lock acquired by client
+
+        # Lock acquired by client
         print_lock.acquire()
 
         print("Connected: ", address[0], ":", address[1])
 
-        # start a new thread
+        # Start a new thread
         start_new_thread(threaded, (client,))
            
-    # close connection
+    # Close connection
     sock.close()
 
 def threaded(client):
     while True:
-        # data received from client
+        # Data received from client
         data = client.recv(data_payload)
+
         if data:
-            # convert received data to string
+            # Convert received data to string
             data_to_string = data.decode()
-            print("Received data: %s" %data_to_string)
-            # send data back
+            print("Received data: %s" % data_to_string)
+
+            # Send data back
             client.send(data)
+
         if not data:
             print("Closing")
-            # release lock on exit
+            # Release lock on exit
             print_lock.release()
 
     client.close()   
